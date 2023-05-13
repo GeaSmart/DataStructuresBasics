dfef17796b9ed7abe788a3d41da936af18cfba93
Gerson Azabache <gerson_dunk@hotmail.com>
2023-05-13 03:29:55 -0500

    lists added

--- DataStructuresBasics/Program.cs
+++ DataStructuresBasics/Program.cs
@@ -3,5 +3,17 @@ Console.WriteLine("=======================================");
 
 //Arrays
 int[] misCalificaciones = { 15, 12, 19, 14, 9 };
+
 string[] amigos = { "Mauricio", "Estefany", "Guillermo", "José", "Rosa" };
 
+string[] misDirecciones = new string[3];
+misDirecciones[0] = "Calle Los Laureles Nro.2432";
+misDirecciones[1] = "Av. Armendáriz 439";
+misDirecciones[2] = "Urb. El Portal grande Mz.T Lt.8";
+
+//Lists
+List<string> misProyectos = new List<string>();
+misProyectos.Add("Sistema de planillas");
+misProyectos.Add("Sistema de ventas");
+misProyectos.Add("Ecommerce");
+misProyectos.Add("Mobile app");
