03ca615026c813eb1fe2dc222166dc087ae11460
Gerson Azabache <gerson_dunk@hotmail.com>
2023-05-13 03:17:58 -0500

    arrays added

--- DataStructuresBasics/Program.cs
+++ DataStructuresBasics/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Identifying the Data Structures in .NET");
+Console.WriteLine("=======================================");
+
+//Arrays
+int[] misCalificaciones = { 15, 12, 19, 14, 9 };
+string[] amigos = { "Mauricio", "Estefany", "Guillermo", "José", "Rosa" };
+
